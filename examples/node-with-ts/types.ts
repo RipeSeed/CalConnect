3ae38ec8667de7f4f1ad18f0144121782a80c64c
HureraNadeem <muhammadhureran8@gmail.com>
2025-01-02 16:16:57 +0500

    chore: added node js with ts code example guide

---
+++ examples/node-with-ts/types.ts
@@ -0,0 +1,32 @@
+export interface CallbackQuery {
+  code?: string;
+}
+
+export interface EventsQuery {
+  startDate?: string;
+  endDate?: string;
+  timezone?: string;
+  calendarId?: string;
+}
+
+export interface CreateEventBody {
+  summary: string;
+  start: string;
+  end: string;
+  timezone: string;
+  description?: string;
+  attendees?: { email: string }[];
+  calendarId?: string;
+}
+
+export interface GoogleCredentials {
+  clientId: string;
+  clientSecret: string;
+  redirectUri: string;
+}
+
+export interface CustomRequest extends Request {
+  user?: {
+    id: string;
+  };
+}
